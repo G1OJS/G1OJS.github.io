c61f734cb322aa8b31fa15f67968fa6531802116
G1OJS <140509227+G1OJS@users.noreply.github.com>
2024-09-27 14:41:31 +0100

    Delete assets/BandSpotsHere/BandSpotsHerePy.bat

--- assets/BandSpotsHere/BandSpotsHerePy.bat
+++
@@ -1,9 +0,0 @@
-@mode 90
-@TITLE BandSpotsHereV1.1.2 by G1OJS
-@REM Specify the Bands, DXCC codes, and max age of spots (in mins) below.
-@REM All spots globally are inspected, but only spot pairs where at least one end of the spot is in these DXCCs are counted
-
-@Python "BandSpotsHereV1.1.2.py"  160m,80m,60m,40m,2m,70cm,6m,4m,10m 223,114,265,122,279,106,294 15
-
-pause
-
